835c19527399edbde9caa488b8c5bfd4a018599c
kevin <Kevin Shi>
2020-07-07 23:24:56 -0700

    start roi feature selection skeleton

--- roi.py
+++ roi.py
@@ -3,6 +3,7 @@ import skimage.io
 import cv2
 from matplotlib import pyplot as plt
 from selective_search import selective_search
+import detect
 
 """
 Utilizes selective search to find the objects in the image
@@ -13,12 +14,13 @@ def s_search(image):
     return boxes
 
 """
-
+Finds features and descriptors(FAD) in master
+Feeds search image into s_search; returned roi
+Find FAD in roi
+Given FAD, check score: score.py
+score cutoff?
+Put all of roi back into search_img parent
+Non Maximum Suppression (NMS)
 """
-def check_imgs(img1, img2):
+def check_roi(master_img, search_img):
     
-
-    # Initiate detector 
-    # FUT: add different detectors
-    dec = cv2.SIFT()
-
---
+++ score.py
@@ -0,0 +1,12 @@
+
+# TODO: do I do cutoff here or elsewhere?
+
+"""
+determines score based on number of good points
+"""
+def basic(kp1, kp2, good):
+    return good
+
+# we can build fancy clustering scoring methods here
+# unsure if the make skeleton code would also be here
+# would probably need some type of class?
