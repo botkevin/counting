bb56e9079565d4084be2af4662914e2d755e81fb
kevin <Kevin Shi>
2020-07-09 20:17:53 -0700

    make check_roi

--- roi.py
+++ roi.py
@@ -5,16 +5,7 @@ from matplotlib import pyplot as plt
 from selective_search import selective_search
 import detect_organized as det
 
-
-def s_search(image):
-    """ 
-    Utilizes selective search to find the objects in the image
-    Returns box and image
-    """
-    boxes = selective_search(image, mode='single')
-    return boxes
-
-"""
+"""    
 Finds features and descriptors(FAD) in master
 Feeds search image into s_search; returned roi
 Find FAD in roi
@@ -23,14 +14,55 @@ score cutoff?
 Put all of roi back into search_img parent
 Non Maximum Suppression (NMS)
 """
+
+def s_search(image):
+    """ 
+    Utilizes selective search to find the objects in the image
+    Returns boxes:(x1,y1,x2,y2)
+    """
+    boxes = selective_search(image, mode='fast')
+    return boxes
+
 def check_roi(master_img, search_img, method):
+    """Finds features and descriptors(FAD) in master
+    Feeds search image into s_search; returned boxes
+    Find FAD in roi
+
+    Parameters
+    ----------
+    master_img : image matrix
+        master image of object to find
+    search_img : 
+        target image with object
+    method : string
+        "orb","surf","sift"
+
+    Returns
+    -------
+    [(box,good), ...]
+        box is the ROI given by selective search
+        good are the points found with detect_organized
+    """
+    # TODO: add jobs/multithreading
     # just use surf as method for now
-    # initiate detector
+
     dec = det.detector(method)
     kp_master, des_master = det.detect(dec, master_img)
+    print("s_search start")
+    boxes = s_search(search_img)
+    print("s_search end")
+    rois = []
+    for box in boxes:
+        mask = make_mask(search_img.shape, box)
+        kp_child, des_child = det.detect(dec, search_img, mask)
+        matches = det.match(des_master, des_child, "FLANN", method)
+        good = det.ratio(matches)
+        rois.append((box,good))
+    return rois
+
+def 
     
-def make_mask(img_shape, rectangle): #TODO: add parameter rectangle
-    # TODO: finish this broken thing
+def make_mask(img_shape, rectangle):
     x1 = rectangle[0]
     y1 = rectangle[1]
     x2 = rectangle[2]
--- score.py
+++ score.py
@@ -1,5 +1,7 @@
 # TODO: do I do cutoff here or elsewhere?
 
+def 
+
 def basic(kp1, kp2, good):
     """
     determines score based on number of good points
@@ -9,3 +11,4 @@ def basic(kp1, kp2, good):
 # we can build fancy clustering scoring methods here
 # unsure if the make skeleton code would also be here
 # would probably need some type of class?
+# TODO: read those papers
